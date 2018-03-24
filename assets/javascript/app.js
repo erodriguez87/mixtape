2493bfa15f6ba1e68c33fcc54ef9e0b907b8bc30
Eddie Rodriguez <erodriguez87@gmail.com>
2018-03-24 15:45:28 -0400

    initial push

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// Mixtape Logic
+// Wait for the page to finish loading
+$(document).ready(function() {
+  var config = {
+    apiKey: "AIzaSyDKWABfmD5z9i_HHVeWAbSxukH1yZqeoAE",
+    authDomain: "susangt2018.firebaseapp.com",
+    databaseURL: "https://susangt2018.firebaseio.com",
+    projectId: "susangt2018",
+    storageBucket: "susangt2018.appspot.com",
+    messagingSenderId: "271189265430"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+ 
+  
+
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Mixtape Generator
+
+A simple app that hooks into bandsintown and spotify to share a playlist of songs and figure out the next time that band is close to you
