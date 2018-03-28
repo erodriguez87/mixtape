0f714973005fa5cd08659e48278c3ff67e94b3de
Eddie Rodriguez <erodriguez87@gmail.com>
2018-03-27 23:38:54 -0400

    working Youtube API and merged the mixtape form into the right div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,12 @@
 // Mixtape Logic
 // Wait for the page to finish loading
+
+// Begin Firebase integration
+// ==================================================================
 $(document).ready(function() {
 
   var config = {
-    apiKey: "AIzaSyDKWABfmD5z9i_HHVeWAbSxukH1yZqeoAE",
+    apikey: "AIzaSyDKWABfmD5z9i_HHVeWAbSxukH1yZqeoAE",
     authDomain: "susangt2018.firebaseapp.com",
     databaseURL: "https://susangt2018.firebaseio.com",
     projectId: "susangt2018",
@@ -12,7 +15,28 @@ $(document).ready(function() {
   };
   firebase.initializeApp(config);
   var database = firebase.database();
- 
-  
+// ==================================================================
+
+
+
+// Youtube API
+// ==================================================================
+// Need to get the trackname and artist passed from music match
+  var trackName; 
+  var artist;
+  var searchString = 'dare you to move';
+  var ytKey = 'AIzaSyC2Ztkch3B2cHJIwLRpZpwzCw4IM6UqwlU';
+
+  var queryURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q='+searchString+'&type=video&maxResults=10&key='+ytKey
+
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(response) {
+    console.log(queryURL);
+    console.log(response);
+  });
+
+// ==================================================================
 
 });
